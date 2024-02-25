43a6d39cc7d3510be35843a0e89a2c48560deb6c
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2024-02-25 17:14:24 +0000

    Initial commit of cross-compiler scripts

---
+++ tools/crosscompiler/buildgcc.sh
@@ -0,0 +1,198 @@
+#!/usr/bin/env bash
+
+###
+### Script for generating os-specific cross compilers using binutils, GCC, and GDB.
+###
+### Requires Docker.
+###
+
+set -o errexit
+set -o nounset
+
+source "$(dirname "${BASH_SOURCE[0]}")/libcrosscompiler.sh"
+
+### DEFAULTS ##################################################################
+
+readonly DEFAULT_ARCH="i686-elf"
+readonly DEFAULT_BINUTILS_VERSION="2.42"
+readonly DEFAULT_GCC_VERSION="13.2.0"
+readonly DEFAULT_GDB_VERSION="14.1"
+
+### USAGE #####################################################################
+
+function buildgcc::usage {
+  echo "USAGE: ${BASH_SOURCE[0]} <flags>"
+  echo "Builds a GCC-based cross-compiler for a given platform, as well as "
+  echo "the corresponding binutils and GDB distributions."
+  echo
+  echo "    --help                Show this message and exit."
+  echo
+  echo "Optional arguments:"
+  echo "    --arch <name>               Set the architecture to build. Defaults to "
+  echo "                                ${DEFAULT_ARCH}."
+  echo "    --binutils <version>        Set the version of binutils to build."
+  echo "                                Defaults to ${DEFAULT_BINUTILS_VERSION}."
+  echo "    --force-rebuild-container   Force rebuild the container if it already"
+  echo "                                exists."
+  echo "    --gcc <version>             Set the version of GCC to build. Defaults"
+  echo "                                to ${DEFAULT_GCC_VERSION}."
+  echo "    --gdb <version>             Set the version of GDB to build. Defaults"
+  echo "                                to ${DEFAULT_GDB_VERSION}."
+}
+
+### ARGUMENT PARSING ##########################################################
+
+arch="${DEFAULT_ARCH}"
+binutils_version="${DEFAULT_BINUTILS_VERSION}"
+force_rebuild_container="false"
+gcc_version="${DEFAULT_GCC_VERSION}"
+gdb_version="${DEFAULT_GDB_VERSION}"
+
+until [[ $# -eq 0 ]]; do
+  case "${1}" in
+
+    --arch)
+      readonly arch="${2}"
+      shift 17
+      ;;
+
+    --binutils)
+      readonly binutils_version="${2}"
+      shift 1
+      ;;
+
+    --force-rebuild-container)
+      readonly force_rebuild_container="true"
+      ;;
+
+    --gcc)
+      readonly gcc_version="${2}"
+      shift 1
+      ;;
+
+    --gdb)
+      readonly gdb_version="${2}"
+      shift 1
+      ;;
+
+    -h|--help)
+      usage
+      exit 0
+      ;;
+
+    *)
+      echo "ERROR: Unknown argument '${1}' provided..."
+      usage
+      exit 1
+      ;;
+
+  esac
+
+  shift 1
+done
+
+### DIRECTORY NAMES ###########################################################
+
+readonly BASE_DIR=target/
+readonly SRC_DIR="src/"
+readonly BIN_DIR="${arch}/bin/"
+
+### FUNCTIONS #################################################################
+
+function buildgcc::prepare_directories {
+  crosscompiler::log "Preparing directories..."
+  mkdir -vp "${BASE_DIR}" "${BASE_DIR}${SRC_DIR}" "${BASE_DIR}${BIN_DIR}"
+}
+
+function buildgcc::download_and_extract_xz {
+  local url="${1?URL}"
+  local tempfile=$(crosscompiler::exec_in_container mktemp)
+  crosscompiler::exec_in_container curl --fail "${url}" -o "${tempfile}"
+  crosscompiler::exec_in_container tar -C "${SRC_DIR}" -xf "${tempfile}"
+}
+
+function buildgcc::build_binutils {
+  buildgcc::download_and_extract_xz "https://ftp.gnu.org/gnu/binutils/binutils-${binutils_version}.tar.xz"
+  crosscompiler::exec_in_container \
+      --env "SRC_DIR=${SRC_DIR}binutils-${binutils_version}" \
+      --env "BIN_DIR=${BIN_DIR}" \
+      --env "TARGET=${arch}" \
+      /bin/bash -o errexit -o xtrace -c '
+        export PREFIX="$(pwd)/${TARGET}"
+        BASEDIR=$(pwd)
+        trap '"'"'mv config.log "${BASEDIR}/binutils-config.log"'"'"' EXIT INT TERM
+
+        cd "$(mktemp -d)"
+        "${BASEDIR}/${SRC_DIR}/configure" \
+            --target "${TARGET}" \
+            --prefix "${PREFIX}" \
+            --with-sysroot \
+            --disable-nls \
+            --disable-werror
+
+        make -j16
+        make install
+      '
+}
+
+function buildgcc::build_gcc {
+  buildgcc::download_and_extract_xz "https://ftp.gnu.org/gnu/gcc/gcc-${gcc_version}/gcc-${gcc_version}.tar.xz"
+  crosscompiler::exec_in_container \
+      --env "SRC_DIR=${SRC_DIR}gcc-${gcc_version}" \
+      --env "BIN_DIR=${BIN_DIR}" \
+      --env "TARGET=${arch}" \
+      /bin/bash -o errexit -o xtrace -c '
+        export PREFIX="$(pwd)/${TARGET}"
+        BASEDIR=$(pwd)
+        trap '"'"'mv config.log "${BASEDIR}/gcc-config.log"'"'"' EXIT INT TERM
+
+        cd "$(mktemp -d)"
+        "${BASEDIR}/${SRC_DIR}/configure" \
+            --target "${TARGET}" \
+            --prefix "${PREFIX}" \
+            --enable-languages=c,c++ \
+            --without-headers \
+            --disable-nls \
+            --disable-werror
+          make -j16 all-gcc
+          make -j16 all-target-libgcc
+          make install-gcc
+          make install-target-libgcc
+      '
+}
+
+function buildgcc::build_gdb {
+  buildgcc::download_and_extract_xz "https://ftp.gnu.org/gnu/gdb/gdb-${gdb_version}.tar.xz"
+  crosscompiler::exec_in_container \
+      --env "SRC_DIR=${SRC_DIR}gdb-${gdb_version}" \
+      --env "BIN_DIR=${BIN_DIR}" \
+      --env "TARGET=${arch}" \
+      /bin/bash -o errexit -o xtrace -c '
+        export PREFIX="$(pwd)/${TARGET}"
+        BASEDIR=$(pwd)
+        trap '"'"'mv config.log "${BASEDIR}/gdb-config.log"'"'"' EXIT INT TERM
+
+        cd "$(mktemp -d)"
+        "${BASEDIR}/${SRC_DIR}/configure" \
+            --target "${TARGET}" \
+            --prefix "${PREFIX}" \
+            --disable-nls \
+            --disable-werror
+
+        make -j16 all-gdb
+        make install-gdb
+      '
+}
+
+### ENTRYPOINT ################################################################
+
+buildgcc::prepare_directories
+
+crosscompiler::build_container_if_exists "${force_rebuild_container}"
+
+readonly container_id=$(crosscompiler::start_container "$(crosscompiler::abspath "${BASE_DIR}")")
+trap 'crosscompiler::stop_container' EXIT INT TERM
+
+buildgcc::build_binutils
+buildgcc::build_gcc
+buildgcc::build_gdb
---
+++ tools/crosscompiler/libcrosscompiler.sh
@@ -0,0 +1,137 @@
+###
+### Facilities for performing platform indepentent cross compilation...
+###
+### Source this script from another script to "import" the functions defined
+### here...
+###
+
+function crosscompiler::abspath {
+  cd "${1?}" && pwd
+}
+
+function crosscompiler::log {
+  printf "\e[1;33m<%s>\e[0;35m %s\e[0m\n" "$(date)" "${1}" >&2
+}
+
+function crosscompiler::container_exists {
+  crosscompiler::log "Checking if build container exists..."
+  docker image ls --format '{{.Repository}}:{{.Tag}}' \
+      | grep -q "${_CONTAINER_NAME}:${_CONTAINER_TAG}"
+}
+
+function crosscompiler::build_container {
+  crosscompiler::log "Creating container to use to build cross compiler..."
+
+  docker build \
+      --compress \
+      --file "${_SCRIPT_DIR}/Dockerfile" \
+      --force-rm \
+      --no-cache \
+      --tag "${_CONTAINER_NAME}:${_CONTAINER_TAG}" \
+      - <<< "${_DOCKERFILE_CONTENT}"
+}
+
+function crosscompiler::build_container_if_exists {
+  # $1 - "true" if forcing the rebuild of the container, "false" otherwise
+
+  if ! crosscompiler::container_exists || [[ "${1?ForceFlag}" == "true" ]]; then
+    crosscompiler::build_container
+  fi
+}
+
+function crosscompiler::start_container {
+  # Start the container and leave it in the background. That way we can exec
+  # into it multiple times to run commands a little more easily than just passing
+  # around singular uber-scripts.
+  #
+  # $1     - working directory to mount and cd into.
+  # stdout - the container ID
+
+  crosscompiler::log "Starting build container in the background to schedule tasks within..."
+
+  local image="${_CONTAINER_NAME}:${_CONTAINER_TAG}"
+  local user="$(id -u "${USER}"):$(id -g "${USER}")"
+  local volume="${1}:${_WORKING_DIR}"
+
+  local container_id
+  container_id=$(docker run \
+      --cgroupns host \
+      --detach \
+      --init \
+      --quiet \
+      --rm \
+      --user "${user}" \
+      --volume "${volume}:Z" \
+      --workdir "${_WORKING_DIR}" \
+      "${image}" \
+      'sleep infinity')
+
+  crosscompiler::log "Created container ${container_id?}"
+  echo "${container_id?}"
+}
+
+function crosscompiler::stop_container {
+  crosscompiler::log "Stopping the build container."
+  docker stop "${container_id?}" > /dev/null
+}
+
+function crosscompiler::exec_in_container {
+  local envvars=()
+  while [[ "${1:-}" == "--env" ]]; do
+    envvars+=(--env "${2?}")
+    shift 2
+  done
+
+  crosscompiler::log "Running ${*@Q}"
+  # Word-split and quote within the same string...
+  docker exec "${envvars[@]}" "${container_id?}" /bin/bash -o errexit -c "${*@Q}"
+}
+
+readonly _CONTAINER_NAME="io.github.ascopes/kimono/crosscompiler"
+readonly _CONTAINER_TAG="latest"
+readonly _DOCKERFILE_CONTENT=$(cat <<-'EOF'
+FROM public.ecr.aws/debian/debian:trixie-slim
+
+RUN apt-get update -yq \
+    && apt-get install -qy --no-install-recommends \
+        bash \
+        bison \
+        build-essential \
+        ca-certificates \
+        curl \
+        flex \
+        gcc \
+        libgmp3-dev \
+        libmpc-dev \
+        libmpfr-dev \
+        tar \
+        texinfo \
+        xz-utils \
+    && apt-get clean autoclean \
+    && apt-get autoremove --yes \
+    && rm -rf /var/lib/apt \
+    && rm -rf /var/lib/dpkg \
+    && rm -rf /var/lib/cache \
+    && rm -rf /var/lib/log
+
+ENTRYPOINT ["/bin/bash", "-c"]
+CMD        ["uname -a && make --version && gcc --version"]
+EOF
+)
+readonly _REQUIREMENTS=(docker grep)
+readonly _SCRIPT_DIR=$(crosscompiler::abspath "$(dirname "${BASH_SOURCE[0]}")")
+readonly _WORKING_DIR="/workspace/"
+
+for required_application in "${_REQUIREMENTS[@]}"; do
+  if ! command -v "${required_application}" :> /dev/null; then
+    echo "ERROR: Please install ${required_application} before continuing..." >&2
+    exit 2
+  fi
+done
+
+if [[ "${DEBUG:-0}" != 0 ]]; then
+  set -o xtrace
+fi
+
+# Ensure we run from the directory using this script.
+cd "${_SCRIPT_DIR}"
